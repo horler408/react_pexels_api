4e0d76f0e270dc2307bf01e736ec74b246cc8092
horler408 <horler.web@gmail.com>
2020-10-22 11:17:02 +0100

    initial clean repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Clean React Page</h1>
+    </div>
+  );
+}
+
+export default App;
