96f2e9a88f7ff0bac7799d8f8d544fbb1a2948b0
horler408 <horler.web@gmail.com>
2020-10-23 21:01:53 +0100

    adding router to pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Nav from './components/NavBar'
+import About from './components/About'
+import FourOFour from './components/FourOFour'
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Nav />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path="*">
+            <FourOFour />
+          </Route>
+        </Switch>
+      </Router>
+        
+        
     </div>
   );
 }
---
+++ src/components/FourOFour.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FourOFour() {
+    return (
+        <div className="App">
+            <h2>404 Not Found</h2>
+        </div>
+    )
+}
