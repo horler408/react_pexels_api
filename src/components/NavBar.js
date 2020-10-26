db2d4cfac7bbd45215baa3f68880418d5991c353
horler408 <horler.web@gmail.com>
2020-10-26 20:53:23 +0100

    adding pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import logo from './../images/log.png'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
     return (
         <div className="nav">
             <img src={logo} alt='Site Logo' />
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/services">Services</Link>
+                </li>
+                <li>
+                    <Link to="/details">Details</Link>
+                </li>
+            </ul>
         </div>
     )
 }
