98511adec7001d363e31323e5dcee039fa5d5ade
horler408 <horler.web@gmail.com>
2020-10-26 20:46:57 +0100

    initial nav fixing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from './../images/log.png'
 
 export default function NavBar() {
     return (
         <div className="nav">
-            
+            <img src={logo} alt='Site Logo' />
         </div>
     )
 }
