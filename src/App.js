2c3e5beafe1f7876dc0db488e00f5f04819b1618
horler408 <horler.web@gmail.com>
2020-10-27 22:27:59 +0100

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-//import Home from './components/Home'
+//import Home from './pages/Home'
 import Home from './junks/HomeJunks'
 import Nav from './components/NavBar'
-import About from './components/About'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contacts from './pages/Contacts'
+import Details from './pages/Details'
 import FourOFour from './components/FourOFour'
 
 function App() {
@@ -18,6 +21,15 @@ function App() {
           </Route>
           <Route path='/about'>
             <About />
+          </Route>
+          <Route path='/services'>
+            <Services />
+          </Route>
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+          <Route path='/details'>
+            <Details />
           </Route>
           <Route path="*">
             <FourOFour />
